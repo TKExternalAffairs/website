3c01ca01e42674d00f754c7ecba7950068d96bb6
Kentuc <103643583+Kentuc322@users.noreply.github.com>
2026-04-14 15:09:06 +0900

    feat: 自動デプロイ機能を追加 (#56)
    * feat: add shared site environment config
    
    * feat: add page metadata for SEO
    
    * feat: deploy site via FTPS
    
    * feat: gate news by site environment
    
    * feat: FTPSアップロードの方法を改善し、FTP-Deploy-Actionを使用
    
    * コメント追加
    
    * local-dirをちゃんとディレクトリにした
    
    * アップロードする先を間違えていたので修正
    
    * /beforedonatingのリダイレクトも追加

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,22 @@
+import type { APIRoute } from "astro";
+
+import { isProductionSite, SITE_URL } from "../lib/site";
+
+export const GET: APIRoute = () => {
+	if (!isProductionSite) {
+		return new Response("User-agent: *\nDisallow: /\n", {
+			headers: {
+				"Content-Type": "text/plain; charset=utf-8",
+			},
+		});
+	}
+
+	return new Response(
+		`User-agent: *\nAllow: /\nSitemap: ${new URL("/sitemap.xml", SITE_URL).toString()}\n`,
+		{
+			headers: {
+				"Content-Type": "text/plain; charset=utf-8",
+			},
+		},
+	);
+};
