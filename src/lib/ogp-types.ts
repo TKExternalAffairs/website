88c466c71c4af346112cc37530015a69542e274a
taichi765 <taichi0209.youtub@gmail.com>
2026-04-30 10:26:57 +0900

    fix: Copilotのsuggestionを適用

--- src/lib/ogp-types.ts
+++ src/lib/ogp-types.ts
@@ -7,9 +7,9 @@ export type ArticleProp =
   | { type: "published_time"; content: Date }
   | { type: "modified_time"; content: Date }
   | { type: "expiration_time"; content: Date }
-  | { type: "author"; content: [string] }
+  | { type: "author"; content: string[] }
   | { type: "section"; content: string }
-  | { type: "tag"; content: [string] };
+  | { type: "tag"; content: string[] };
 
 /**
  * @see https://ogp.me/#types
